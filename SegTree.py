e1af0d5c2ee2c063bb7cbe84b68008ce6b5bfc4a
lucasyu17 <ben0107@sjtu.edu.cn>
2019-01-24 21:58:10 +0800

    segment tree

--- SegTree.py
+++ SegTree.py
@@ -6,46 +6,43 @@ class SegTree(object):
     """Use python list to build the segmented tree structure.
        Basic idea is to separate priority data and the tree in two lists."""
     def __init__(self, capacity):
-        self.tree = np.zeros(shape=[2*capacity+1, 1])  # tree structure
-        self.data = np.zeros(shape=[capacity, 1], dtype=object)  # record all transitions
+        self.tree = np.zeros(2*capacity-1)  # tree structure
+        self.data = np.zeros(capacity, dtype=object)  # record all transitions
         self.data_point = 0
         self.capacity = capacity
 
     def add(self, data, new_priority):
         self.data[self.data_point] = data
         tree_index = self.data_point + self.capacity - 1
-        self.data_point = self.data_point + 1
-        if self.data_point > len(self.data):
+        self.update(new_priority=new_priority, tree_index=tree_index)
+        print ("data point: ", self.data_point, " ;p: ", new_priority)
+        self.data_point += 1
+        if self.data_point > self.capacity:
             self.data_point = 0
-        change = new_priority - self.tree[tree_index]
-        self.update(change=change, tree_index=tree_index)
 
-    def update(self, change, tree_index):
-        while True:
+    def update(self, new_priority, tree_index):
+        change = new_priority - self.tree[tree_index]
+        self.tree[tree_index] = new_priority
+        while tree_index != 0:
+            tree_index = (tree_index - 1) // 2
             self.tree[tree_index] += change
-            parent = (tree_index - 1) // 2
-            if parent < 0:
-                break
-            else:
-                self.tree[parent] += change
 
     def get_sum(self):
         return self.tree[0]
 
 
-class test_data(object):
-    def __init__(self, data):
-        self.data = data
-
-
 if __name__ == '__main__':
     test_sum_tree = SegTree(capacity=10)
-    for i in range(9):
+    for i in range(1, 11):
         # data = test_data([0, 0, 0, 0])
-        data = 0
-        test_sum_tree.add(data, i/9)
+        data = 0.1
+        test_sum_tree.add(data, float(i)/10)
 
-    print ("sum: ", test_sum_tree.get_sum())
+    print "tree: ", test_sum_tree.tree
+    for j in range(test_sum_tree.capacity-1):
+        print "parent: ", test_sum_tree.tree[j]
+        print("left son: ", test_sum_tree.tree[2*j+1], " ; right son: ", test_sum_tree.tree[2*j+2])
+    print "sum: ", test_sum_tree.get_sum()
 
 
 
