4fc70b09a35f5f55438cee262a7940056f63970d
lucasyu17 <18818271872@163.com>
2019-01-26 17:21:18 +0800

    record reward and use priori replay buffer

--- ddpg.py
+++ ddpg.py
@@ -6,10 +6,14 @@ from CriticNetwork import CriticNetwork
 from OU import OU
 from ReplayBuffer import ReplayBuffer
 from gym_torcs import TorcsEnv
+import time
+import os
+import math
+from PrioritizedReplayBuffer import PrioritizedReplayBuff
 
 OU = OU()  # Ornstein-Uhlenbeck Process
 
-BUFFER_SIZE = 100000
+BUFFER_SIZE = math.pow(2, 17)
 BATCH_SIZE = 32
 GAMMA = 0.99
 TAU = 0.001  # Target Network HyperParameters
@@ -18,7 +22,12 @@ LRC = 0.001  # Lerning rate for Critic
 EXPLORE = 100000.
 
 
-def play_game(train_indicator=1):  # 1 means Train, 0 means simply Run
+def play_game(train_indicator=1, priori_replay_buff=False):
+    """
+    :param train_indicator: 1 means Train, 0 means simply Run
+    :param priori_replay_buff: 1 means use prioritized replay buffer, 0 means use normal replay buffer
+    :return:
+    """
     action_dim = 3  # Steering/Acceleration/Brake
     state_dim = 29  # of sensors input
 
@@ -40,114 +49,126 @@ def play_game(train_indicator=1):  # 1 means Train, 0 means simply Run
 
     actor = ActorNetwork(sess, state_dim, action_dim, BATCH_SIZE, TAU, LRA)
     critic = CriticNetwork(sess, state_dim, action_dim, BATCH_SIZE, TAU, LRC)
-    buff = ReplayBuffer(BUFFER_SIZE)  # Create replay buffer
+    if not priori_replay_buff:
+        buff = ReplayBuffer(BUFFER_SIZE)  # Create replay buffer
+    else:
+        buff = PrioritizedReplayBuff(BUFFER_SIZE)
 
     # Generate a Torcs environment
     env = TorcsEnv(vision=vision, throttle=True, gear_change=False)
 
-    # Now load the weight
-    print("Now we load the weight")
-    try:
-        actor.model.load_weights("actormodel.h5")
-        critic.model.load_weights("criticmodel.h5")
-        actor.target_model.load_weights("actormodel.h5")
-        critic.target_model.load_weights("criticmodel.h5")
-        print("Weight load successfully")
-    except:
-        print("Cannot find the weight")
+    # csv file to record the average reward of every episode
+    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    dir_name = 'avg_rwd_data'
+    filename = 'avg_reward_episode_' + time_str + '.csv'
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    filename = os.path.join(dir_name, filename)
+    with open(filename, mode='w') as csv_file:
+        # Now load the weight
+        print("Now we load the weight")
+        try:
+            actor.model.load_weights("actormodel.h5")
+            critic.model.load_weights("criticmodel.h5")
+            actor.target_model.load_weights("actormodel.h5")
+            critic.target_model.load_weights("criticmodel.h5")
+            print("Weight load successfully")
+        except:
+            print("Cannot find the weight")
 
-    print("TORCS Experiment Start...")
-    for i in range(episode_count):
+        print("TORCS Experiment Start...")
+        for i in range(episode_count):
 
-        print("Episode : " + str(i) + " Replay Buffer " + str(buff.count()))
+            print("Episode : " + str(i) + " Replay Buffer " + str(buff.count()))
 
-        if np.mod(i, 3) == 0:
-            ob = env.reset(relaunch=True)  # relaunch TORCS every 3 episode because of the memory leak error
-        else:
-            ob = env.reset()
-        s_t = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY, ob.speedZ,
-                         ob.wheelSpinVel / 100.0, ob.rpm))
+            if np.mod(i, 3) == 0:
+                ob = env.reset(relaunch=True)  # relaunch TORCS every 3 episode because of the memory leak error
+            else:
+                ob = env.reset()
+            s_t = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY, ob.speedZ,
+                             ob.wheelSpinVel / 100.0, ob.rpm))
 
-        total_reward = 0.
-        for j in range(max_steps):
-            loss = 0
-            epsilon -= 1.0 / EXPLORE
-            a_t = np.zeros([1, action_dim])
-            noise_t = np.zeros([1, action_dim])
+            total_reward = 0.
+            for j in range(max_steps):
+                loss = 0
+                epsilon -= 1.0 / EXPLORE
+                a_t = np.zeros([1, action_dim])
+                noise_t = np.zeros([1, action_dim])
 
-            a_t_original = actor.model.predict(s_t.reshape(1, s_t.shape[0]))
-            # shape: [s_t.shape[0]] --> [[s_t.shape[0]]
-            noise_t[0][0] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][0], 0.0, 0.60, 0.30)
-            noise_t[0][1] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][1], 0.5, 1.00, 0.10)
-            noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2], -0.1, 1.00, 0.05)
+                a_t_original = actor.model.predict(s_t.reshape(1, s_t.shape[0]))
+                # shape: [s_t.shape[0]] --> [[s_t.shape[0]]
+                noise_t[0][0] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][0], 0.0, 0.60, 0.30)
+                noise_t[0][1] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][1], 0.5, 1.00, 0.10)
+                noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2], -0.1, 1.00, 0.05)
 
-            # The following code do the stochastic brake
-            # if random.random() <= 0.1:
-            #    print("********Now we apply the brake***********")
-            #    noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2],  0.2 , 1.00, 0.10)
+                # The following code do the stochastic brake
+                # if random.random() <= 0.1:
+                #    print("********Now we apply the brake***********")
+                #    noise_t[0][2] = train_indicator * max(epsilon, 0) * OU.function(a_t_original[0][2],  0.2 , 1.00, 0.10)
 
-            a_t[0][0] = a_t_original[0][0] + noise_t[0][0]
-            a_t[0][1] = a_t_original[0][1] + noise_t[0][1]
-            a_t[0][2] = a_t_original[0][2] + noise_t[0][2]
+                a_t[0][0] = a_t_original[0][0] + noise_t[0][0]
+                a_t[0][1] = a_t_original[0][1] + noise_t[0][1]
+                a_t[0][2] = a_t_original[0][2] + noise_t[0][2]
 
-            ob, r_t, done, info = env.step(a_t[0])
-            s_t1 = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY, ob.speedZ,
-                              ob.wheelSpinVel / 100.0, ob.rpm))
+                ob, r_t, done, info = env.step(a_t[0])
+                s_t1 = np.hstack((ob.angle, ob.track, ob.trackPos, ob.speedX, ob.speedY, ob.speedZ,
+                                  ob.wheelSpinVel / 100.0, ob.rpm))
 
-            # Add replay buffer: (state_t, action_t, reward_t, state_t+1, end_or_not)
-            buff.add(s_t, a_t[0], r_t, s_t1, done)
+                # Add replay buffer: (state_t, action_t, reward_t, state_t+1, end_or_not)
+                buff.add(s_t, a_t[0], r_t, s_t1, done)
 
-            # Do the batch update
-            batch = buff.getBatch(BATCH_SIZE)
-            states = np.asarray([e[0] for e in batch])
-            actions = np.asarray([e[1] for e in batch])
-            rewards = np.asarray([e[2] for e in batch])
-            new_states = np.asarray([e[3] for e in batch])
-            dones = np.asarray([e[4] for e in batch])
-            y_t = np.asarray([e[1] for e in batch])
+                # Do the batch update
+                batch = buff.getBatch(BATCH_SIZE)
+                states = np.asarray([e[0] for e in batch])
+                actions = np.asarray([e[1] for e in batch])
+                rewards = np.asarray([e[2] for e in batch])
+                new_states = np.asarray([e[3] for e in batch])
+                dones = np.asarray([e[4] for e in batch])
+                y_t = np.asarray([e[1] for e in batch])
 
-            target_q_values = critic.target_model.predict([new_states, actor.target_model.predict(new_states)])
+                target_q_values = critic.target_model.predict([new_states, actor.target_model.predict(new_states)])
 
-            for k in range(len(batch)):
-                if dones[k]:
-                    y_t[k] = rewards[k]
-                else:
-                    y_t[k] = rewards[k] + GAMMA * target_q_values[k]
+                for k in range(len(batch)):
+                    if dones[k]:
+                        y_t[k] = rewards[k]
+                    else:
+                        y_t[k] = rewards[k] + GAMMA * target_q_values[k]
 
-            if train_indicator:
-                loss += critic.model.train_on_batch([states, actions], y_t)
-                action_for_grad = actor.model.predict(states)
-                grads = critic.gradients(states, action_for_grad)
-                actor.train(states, grads)
-                actor.target_train()
-                critic.target_train()
+                if train_indicator:
+                    loss += critic.model.train_on_batch([states, actions], y_t)
+                    action_for_grad = actor.model.predict(states)
+                    grads = critic.gradients(states, action_for_grad)
+                    actor.train(states, grads)
+                    actor.target_train()
+                    critic.target_train()
 
-            total_reward += r_t
-            s_t = s_t1
+                total_reward += r_t
+                s_t = s_t1
 
-            print("Episode", i, "Step", step, "Action", a_t, "Reward", r_t, "Loss", loss)
+                print("Episode", i, "Step", step, "Action", a_t, "Reward", r_t, "Loss", loss)
+                step += 1
+                if done:
+                    break
 
-            step += 1
-            if done:
-                break
+            if np.mod(i, 3) == 0:
+                if train_indicator:
+                    print("Now we save model")
+                    actor.model.save_weights("actormodel.h5", overwrite=True)
+                    with open("actormodel.json", "w") as outfile:
+                        json.dump(actor.model.to_json(), outfile)
 
-        if np.mod(i, 3) == 0:
-            if train_indicator:
-                print("Now we save model")
-                actor.model.save_weights("actormodel.h5", overwrite=True)
-                with open("actormodel.json", "w") as outfile:
-                    json.dump(actor.model.to_json(), outfile)
+                    critic.model.save_weights("criticmodel.h5", overwrite=True)
+                    with open("criticmodel.json", "w") as outfile:
+                        json.dump(critic.model.to_json(), outfile)
 
-                critic.model.save_weights("criticmodel.h5", overwrite=True)
-                with open("criticmodel.json", "w") as outfile:
-                    json.dump(critic.model.to_json(), outfile)
-
-        print("TOTAL REWARD @ " + str(i) + "-th Episode  : Reward " + str(total_reward))
-        print("Total Step: " + str(step))
-        print("")
-
-    env.end()  # This is for shutting down TORCS
-    print("Finish.")
+            print("TOTAL REWARD @ " + str(i) + "-th Episode  : Reward " + str(total_reward))
+            csv_file.write(str(total_reward / step))
+            csv_file.write(",")
+            print("Total Step: " + str(step))
+            print("")
+        csv_file.close()
+        env.end()  # This is for shutting down TORCS
+        print("Finish.")
 
 
 if __name__ == "__main__":
